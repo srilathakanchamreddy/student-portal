c7308faf66c06998487d381e1477c09e8384ade2
srilatha <srilathakanchamreddy26@gmail.com>
2026-08-16 00:52:07 +0530

    Add initial student portal application

---
+++ src/StudentPortal.java
@@ -0,0 +1,11 @@
+public class StudentPortal {
+
+    public static void main(String[] args) {
+        String studentName = "Vinay";
+        String course = "M.Tech CSE";
+
+        System.out.println("Welcome to Student Portal");
+        System.out.println("Student: " + studentName);
+        System.out.println("Course: " + course);
+    }
+}
