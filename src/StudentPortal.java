847043a026db4c96310096528fd0b52e076bf42b
srilatha <srilathakanchamreddy26@gmail.com>
2026-08-16 11:27:55 +0530

    Integrate login service with student portal

--- src/StudentPortal.java
+++ src/StudentPortal.java
@@ -1,10 +1,21 @@
 public class StudentPortal {
 
     public static void main(String[] args) {
-        StudentRecord student = new StudentRecord(101, "Vinay", "M.Tech CSE");
+        StudentRecord student =
+                new StudentRecord(101, "Vinay", "M.Tech CSE");
+
+        LoginService loginService = new LoginService();
+
+        boolean authenticated =
+                loginService.authenticate("student", "portal123");
 
         System.out.println("Welcome to Student Portal");
+
+        System.out.println(
+                loginService.getLoginMessage(authenticated)
+        );
+
         student.displayDetails();
-	student.displayStatus();
+        student.displayStatus();
     }
 }
