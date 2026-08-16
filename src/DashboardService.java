bd7f2154683801f5f3361e40e975a6d2fc907a39
srilatha <srilathakanchamreddy26@gmail.com>
2026-08-16 12:16:47 +0530

    Add dashboard login status

--- src/DashboardService.java
+++ src/DashboardService.java
@@ -6,4 +6,8 @@ public class DashboardService {
         System.out.println("Student Name: " + student.getName());
         System.out.println("Course: " + student.getCourse());
     }
+public void displayLoginStatus(boolean authenticated) {
+    System.out.println("Login Status: "
+            + (authenticated ? "Active" : "Inactive"));
+}
 }
