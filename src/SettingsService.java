bafc4ae3ad0e35d49759802ceca0cee62bb45cf7
srilatha <srilathakanchamreddy26@gmail.com>
2026-08-16 13:38:50 +0530

    Add student settings service

---
+++ src/SettingsService.java
@@ -0,0 +1,8 @@
+public class SettingsService {
+
+    public void displaySettings() {
+        System.out.println("===== Student Settings =====");
+        System.out.println("Notifications: Enabled");
+        System.out.println("Theme: Default");
+    }
+}
